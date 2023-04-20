7e7e1adac883f3941ef2a7240ae8ac1e25bb8279
Squirrel18 <daniel.quiroga@edunext.co>
2023-04-20 18:14:07 -0500

    feat: allow CCX coaches and instructors to grade assignments. docs: add custom features documentation.

--- edx_sga/sga.py
+++ edx_sga/sga.py
@@ -26,6 +26,7 @@ from django.utils.translation import gettext as _
 from lms.djangoapps.courseware.models import StudentModule
 from openedx.core.lib.safe_lxml import etree
 from common.djangoapps.student.models import user_by_anonymous_id
+from ccx_keys.locator import CCXLocator
 from submissions import api as submissions_api
 from submissions.models import StudentItem as SubmissionsStudent
 from submissions.models import Submission
@@ -911,9 +912,12 @@ class StaffGradedAssignmentXBlock(
         """
         Check if user role is instructor.
         """
-        if user_service := self.runtime.service(self, 'user'):
-            return user_service.get_current_user().opt_attrs.get(ATTR_KEY_USER_ROLE) == "instructor"
-        return False
+        # If it's a CCX course and the user has staff role (ccx_coach) or admin role (instructor)
+        # the user must be able to grade assignments without instructor approval.
+        if isinstance(self.course_id, CCXLocator):
+            return self.xmodule_runtime.get_user_role() in ('staff', 'instructor')
+
+        return self.xmodule_runtime.get_user_role() == "instructor"
 
     def show_staff_grading_interface(self):
         """
